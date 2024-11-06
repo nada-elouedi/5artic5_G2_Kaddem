85ffc827a954e497aefbde9f1e56855e403885e8
RaniaMatrouh <124463393+RaniaMatrouh@users.noreply.github.com>
2024-11-06 15:21:06 +0100

    Update KaddemApplication.java

--- src/main/java/tn/esprit/spring/kaddem/KaddemApplication.java
+++ src/main/java/tn/esprit/spring/kaddem/KaddemApplication.java
@@ -2,14 +2,14 @@ package tn.esprit.spring.kaddem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springdoc.core.annotations.OpenAPIDefinition;
-import org.springdoc.core.annotations.Info;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @SpringBootApplication
-@OpenAPIDefinition(info = @Info(title = "Kaddem API", version = "v1", description = "Documentation de l'API Kaddem"))
 public class KaddemApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(KaddemApplication.class, args);
     }
+
 }
