f3a44eab68fab312cbab518c4750ceed090c6113
RaniaMatrouh <124463393+RaniaMatrouh@users.noreply.github.com>
2024-11-06 14:27:57 +0100

    Update Equipe.java

--- src/main/java/tn/esprit/spring/kaddem/entities/Equipe.java
+++ src/main/java/tn/esprit/spring/kaddem/entities/Equipe.java
@@ -2,17 +2,26 @@ package tn.esprit.spring.kaddem.entities;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.io.Serializable;
 import java.util.HashSet;
 import java.util.Set;
 
 @Entity
+@Getter
+@Setter
+@NoArgsConstructor // Constructeur sans paramètres
+@AllArgsConstructor
 public class Equipe implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer idEquipe;
     private String nomEquipe;
+
     @Enumerated(EnumType.STRING)
     private Niveau niveau;
 
@@ -23,13 +32,12 @@ public class Equipe implements Serializable {
     @OneToOne
     private DetailEquipe detailEquipe;
 
-    // Constructeur par défaut
-    public Equipe(int id, String nomEquipe) {
-   this.idEquipe = id;
-     this.nomEquipe = nomEquipe;
+    // Constructeur avec paramètres
+    public Equipe(Integer idEquipe, String nomEquipe) {
+        this.idEquipe = idEquipe;
+        this.nomEquipe = nomEquipe;
     }
 
-    // Autres constructeurs
     public Equipe(String nomEquipe) {
         this.nomEquipe = nomEquipe;
     }
@@ -39,19 +47,6 @@ public class Equipe implements Serializable {
         this.niveau = niveau;
     }
 
-    public Equipe(Integer idEquipe, String nomEquipe, Niveau niveau) {
-        this.idEquipe = idEquipe;
-        this.nomEquipe = nomEquipe;
-        this.niveau = niveau;
-    }
-
-    public Equipe(String nomEquipe, Niveau niveau, Set<Etudiant> etudiants, DetailEquipe detailEquipe) {
-        this.nomEquipe = nomEquipe;
-        this.niveau = niveau;
-        this.etudiants = etudiants;
-        this.detailEquipe = detailEquipe;
-    }
-
     public Equipe(Integer idEquipe, String nomEquipe, Niveau niveau, Set<Etudiant> etudiants, DetailEquipe detailEquipe) {
         this.idEquipe = idEquipe;
         this.nomEquipe = nomEquipe;
@@ -59,46 +54,4 @@ public class Equipe implements Serializable {
         this.etudiants = etudiants;
         this.detailEquipe = detailEquipe;
     }
-
-    // Getters et Setters
-    public Integer getIdEquipe() {
-        return idEquipe;
-    }
-
-    public void setIdEquipe(Integer idEquipe) {
-        this.idEquipe = idEquipe;
-    }
-
-    public String getNomEquipe() {
-        return nomEquipe;
-    }
-
-    public void setNomEquipe(String nomEquipe) {
-        this.nomEquipe = nomEquipe;
-    }
-
-    public Niveau getNiveau() {
-        return niveau;
-    }
-
-    public void setNiveau(Niveau niveau) {
-        this.niveau = niveau;
-    }
-
-    public Set<Etudiant> getEtudiants() {
-        return etudiants;
-    }
-
-    public void setEtudiants(Set<Etudiant> etudiants) {
-        this.etudiants = etudiants;
-    }
-
-    public DetailEquipe getDetailEquipe() {
-        return detailEquipe;
-    }
-
-    public void setDetailEquipe(DetailEquipe detailEquipe) {
-        this.detailEquipe = detailEquipe;
-    }
-
 }
