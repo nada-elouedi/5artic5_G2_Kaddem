808fa5a82c546cd1bba732aa9c4872dc4aa9fb0d
RaniaMatrouh <124463393+RaniaMatrouh@users.noreply.github.com>
2024-11-06 15:03:02 +0100

    Update KaddemApplication.java

--- src/main/java/tn/esprit/spring/kaddem/KaddemApplication.java
+++ src/main/java/tn/esprit/spring/kaddem/KaddemApplication.java
@@ -3,6 +3,11 @@ package tn.esprit.spring.kaddem;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.context.annotation.Bean;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
 
 @EnableScheduling
 @SpringBootApplication
