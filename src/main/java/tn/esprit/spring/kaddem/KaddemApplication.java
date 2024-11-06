865c9a53df68401b2ce593a247963ce6ddb3140e
RaniaMatrouh <124463393+RaniaMatrouh@users.noreply.github.com>
2024-11-06 15:12:07 +0100

    Update KaddemApplication.java

--- src/main/java/tn/esprit/spring/kaddem/KaddemApplication.java
+++ src/main/java/tn/esprit/spring/kaddem/KaddemApplication.java
@@ -2,28 +2,14 @@ package tn.esprit.spring.kaddem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.context.annotation.Bean;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
+import org.springdoc.core.annotations.OpenAPIDefinition;
+import org.springdoc.core.annotations.Info;
 
-@EnableScheduling
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "Kaddem API", version = "v1", description = "Documentation de l'API Kaddem"))
 public class KaddemApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(KaddemApplication.class, args);
     }
-    @Bean
-    public Docket api() {
-        return new Docket(DocumentationType.SWAGGER_2)
-                .select()
-                .apis(RequestHandlerSelectors.basePackage("tn.esprit.spring.kaddem"))  // Le package de ton application
-                .paths(PathSelectors.any())  // Active toutes les routes
-                .build();
-    }
-
-
 }
