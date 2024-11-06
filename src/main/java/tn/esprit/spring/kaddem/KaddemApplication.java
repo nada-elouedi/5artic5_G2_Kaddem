6d7e347b0aa97962e8bf72f49bd05f8030060abd
RaniaMatrouh <124463393+RaniaMatrouh@users.noreply.github.com>
2024-11-06 15:00:36 +0100

    Update KaddemApplication.java

--- src/main/java/tn/esprit/spring/kaddem/KaddemApplication.java
+++ src/main/java/tn/esprit/spring/kaddem/KaddemApplication.java
@@ -11,5 +11,14 @@ public class KaddemApplication {
     public static void main(String[] args) {
         SpringApplication.run(KaddemApplication.class, args);
     }
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("tn.esprit.spring.kaddem"))  // Le package de ton application
+                .paths(PathSelectors.any())  // Active toutes les routes
+                .build();
+    }
+
 
 }
